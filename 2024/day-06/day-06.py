531a003afc4341b7f82c585754f18280d9e678e3
Toby Law <toby.law@rokt.com>
2024-12-09 09:03:48 +1100

    2024-6-2 optimised
    Got the runtime down to 6s by checking only tiles on the path

--- 2024/day-06/day-06.py
+++ 2024/day-06/day-06.py
@@ -1,46 +1,52 @@
 import sys
 
-sys.setrecursionlimit(10**6)
 lines = [line.strip() for line in sys.stdin]
 
-R = len(lines)
+grid = [[c for c in line] for line in lines]
+R = len(grid)
 assert R > 0
-C = len(lines[0])
+C = len(grid[0])
 
 dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-p1 = 0
-p2 = 0
+
+
+def walk(grid):
+    r, c, d = sr, sc, 0
+    visited = set()
+    seen = set()
+    while True:
+        if (r, c, d) in visited:
+            return seen, True
+
+        visited.add((r, c, d))
+        seen.add((r, c))
+
+        dr, dc = dirs[d]
+        nr, nc = r + dr, c + dc
+
+        if not (0 <= nr < R and 0 <= nc < C):
+            return seen, False
+        if grid[nr][nc] == "#":
+            d = (d + 1) % 4
+        else:
+            r, c = nr, nc
 
 
 sr, sc = 0, 0
 for r in range(R):
     for c in range(C):
-        if lines[r][c] == "^":
+        if grid[r][c] == "^":
             sr, sc = r, c
 
-for tr in range(R):
-    for tc in range(C):
-        visited = set()
-        seen = set()
-        r, c, d = sr, sc, 0
-        while True:
-            if (r, c, d) in visited:
-                p2 += 1
-                break
-
-            visited.add((r, c, d))
-            seen.add((r, c))
-
-            dr, dc = dirs[d]
-            nr, nc = r + dr, c + dc
-
-            if not (0 <= nr < R and 0 <= nc < C):
-                p1 = len(seen)
-                break
-            if lines[nr][nc] == "#" or (nr, nc) == (tr, tc):
-                d = (d + 1) % 4
-            else:
-                r, c = nr, nc
-
+path = walk(grid)[0]
+p1 = len(path)
 print(p1)
+
+p2 = 0
+for r, c in path:
+    ch = grid[r][c]
+    grid[r][c] = "#"
+    p2 += walk(grid)[1]
+    grid[r][c] = ch
+
 print(p2)
